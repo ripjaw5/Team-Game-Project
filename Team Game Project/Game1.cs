9b5441eb8e897302b57ca9f6e7be410f924a6b4c
Omar Jafry <216841@friscoisd.org>
2023-03-24 14:20:24 -0500

    n

--- Team Game Project/Game1.cs
+++ Team Game Project/Game1.cs
@@ -7,9 +7,16 @@ namespace Team_Game_Project
 {
     public class Game1 : Game
     {
+        enum GameState
+        {
+            startScreen,
+            overworld,
+            battle
+        }
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private Texture2D _icons;
+        private GameState _state;
         private Rectangle _screen;
         private Texture2D _player;
         private Rectangle[] _playerSrc;
@@ -28,6 +35,7 @@ namespace Team_Game_Project
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
             _graphics.ApplyChanges();
+            _state = GameState.startScreen;
             _activeMap = 0;
             _playerSrc = new Rectangle[3];
             _batSrc = new Rectangle[6];
@@ -67,103 +75,130 @@ namespace Team_Game_Project
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
+            
             // TODO: Add your update logic here
             KeyboardState kb = Keyboard.GetState();
-            if (kb.IsKeyDown(Keys.LeftShift))
-                _sprint = true;
-            else
-                _sprint = false;
-            if (!_sprint)
+            if (_state == GameState.startScreen)
             {
-                if (kb.IsKeyDown(Keys.Up) && _pos.Y > 0)
+                if (kb.IsKeyDown(Keys.Space))
                 {
-                    _activePlayer = 2;
-                    _pos.Y -= 2;
-                }
-                else if (kb.IsKeyDown(Keys.Up) && _pos.Y <= 0 && _activeMap == 0)
-                {
-                    _activeMap = 1;
-                    _pos.Y = _screen.Height - 48;
-                }
-                if (kb.IsKeyDown(Keys.Down) && _pos.Y < _screen.Height - 48)
-                {
-                    _activePlayer = 1;
-                    _pos.Y += 2;
-                }
-                else if (kb.IsKeyDown(Keys.Down) && _pos.Y >= _screen.Height - 48 && _activeMap == 1)
-                {
-                    _activeMap = 0;
-                    _pos.Y = 0;
-                }
-                if (kb.IsKeyDown(Keys.Left) && _pos.X >= 0)
-                {
-                    _pos.X -= 2;
-                }
-                if (kb.IsKeyDown(Keys.Right) && _pos.X <= _screen.Width - 48)
-                {
-                    _pos.X += 2;
+                    _state = GameState.overworld;
                 }
             }
-            else
+            else if (_state == GameState.overworld)
             {
-                if (kb.IsKeyDown(Keys.Up) && _pos.Y > 0)
+                if (kb.IsKeyDown(Keys.LeftAlt))
+                    _state = GameState.battle;
+                if (kb.IsKeyDown(Keys.LeftShift))
+                    _sprint = true;
+                else
+                    _sprint = false;
+                if (!_sprint)
                 {
-                    _pos.Y -= 8;
+                    if (kb.IsKeyDown(Keys.Up) && _pos.Y > 0)
+                    {
+                        _activePlayer = 2;
+                        _pos.Y -= 2;
+                    }
+                    else if (kb.IsKeyDown(Keys.Up) && _pos.Y <= 0 && _activeMap == 0)
+                    {
+                        _activeMap = 1;
+                        _pos.Y = _screen.Height - 48;
+                    }
+                    if (kb.IsKeyDown(Keys.Down) && _pos.Y < _screen.Height - 48)
+                    {
+                        _activePlayer = 1;
+                        _pos.Y += 2;
+                    }
+                    else if (kb.IsKeyDown(Keys.Down) && _pos.Y >= _screen.Height - 48 && _activeMap == 1)
+                    {
+                        _activeMap = 0;
+                        _pos.Y = 0;
+                    }
+                    if (kb.IsKeyDown(Keys.Left) && _pos.X >= 0)
+                    {
+                        _pos.X -= 2;
+                    }
+                    if (kb.IsKeyDown(Keys.Right) && _pos.X <= _screen.Width - 48)
+                    {
+                        _pos.X += 2;
+                    }
                 }
-                else if (kb.IsKeyDown(Keys.Up) && _pos.Y <= 0 && _activeMap == 0)
+                else
                 {
-                    _activeMap = 1;
-                    _pos.Y = _screen.Height - 48;
-                }
-                if (kb.IsKeyDown(Keys.Down) && _pos.Y < _screen.Height - 48)
-                {
-                    _pos.Y += 8;
-                }
-                else if (kb.IsKeyDown(Keys.Down) && _pos.Y >= _screen.Height - 48 && _activeMap == 1)
-                {
-                    _activeMap = 0;
-                    _pos.Y = 0;
-                }
-                if (kb.IsKeyDown(Keys.Left) && _pos.X >= 0)
-                {
-                    _isLeft = true;
-                    _pos.X -= 8;
-                }
-                if (kb.IsKeyDown(Keys.Right) && _pos.X <= _screen.Width - 48)
-                {
-                    _isLeft = false;
-                    _pos.X += 8;
-                }
-
-                _activeBat += .25;
-                if (_activeBat >= 6)
-                {
-                    _activeBat = 0;
+                    if (kb.IsKeyDown(Keys.Up) && _pos.Y > 0)
+                    {
+                        _pos.Y -= 8;
+                    }
+                    else if (kb.IsKeyDown(Keys.Up) && _pos.Y <= 0 && _activeMap == 0)
+                    {
+                        _activeMap = 1;
+                        _pos.Y = _screen.Height - 48;
+                    }
+                    if (kb.IsKeyDown(Keys.Down) && _pos.Y < _screen.Height - 48)
+                    {
+                        _pos.Y += 8;
+                    }
+                    else if (kb.IsKeyDown(Keys.Down) && _pos.Y >= _screen.Height - 48 && _activeMap == 1)
+                    {
+                        _activeMap = 0;
+                        _pos.Y = 0;
+                    }
+                    if (kb.IsKeyDown(Keys.Left) && _pos.X >= 0)
+                    {
+                        _isLeft = true;
+                        _pos.X -= 8;
+                    }
+                    if (kb.IsKeyDown(Keys.Right) && _pos.X <= _screen.Width - 48)
+                    {
+                        _isLeft = false;
+                        _pos.X += 8;
+                    }
+                    _activeBat += .25;
+                    if (_activeBat >= 6)
+                    {
+                        _activeBat = 0;
+                    }
                 }
             }
+            else if (_state == GameState.battle)
+            {
 
+            }
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            if (_activeMap == 0)
-                GraphicsDevice.Clear(Color.White);
-            else if (_activeMap == 1)
-                GraphicsDevice.Clear(Color.Black);
-            else
+            if (_state == GameState.startScreen)
+            {
                 GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            }
+            else
+            {
+                if (_activeMap == 0)
+                    GraphicsDevice.Clear(Color.White);
+                else if (_activeMap == 1)
+                    GraphicsDevice.Clear(Color.Black);
+                else
+                    GraphicsDevice.Clear(Color.CornflowerBlue);
+            }
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
-            //_spriteBatch.Draw(_player, new Vector2(0, 0), null, Color.White, 0, new Vector2(0, 0), 5, SpriteEffects.None, 0);
-            if (!_sprint)
-                _spriteBatch.Draw(_player, _pos, _playerSrc[_activePlayer], Color.White);
-            else if (_isLeft)
-                _spriteBatch.Draw(_bat, _pos, _batSrc[(int) _activeBat], Color.White, 0, new Vector2(), SpriteEffects.FlipHorizontally, 0);
-            else
-                _spriteBatch.Draw(_bat, _pos, _batSrc[(int) _activeBat], Color.White);
+            if (_state == GameState.overworld)
+            {
+                if (!_sprint)
+                    _spriteBatch.Draw(_player, _pos, _playerSrc[_activePlayer], Color.White);
+                else if (_isLeft)
+                    _spriteBatch.Draw(_bat, _pos, _batSrc[(int)_activeBat], Color.White, 0, new Vector2(), SpriteEffects.FlipHorizontally, 0);
+                else
+                    _spriteBatch.Draw(_bat, _pos, _batSrc[(int)_activeBat], Color.White);
+            }
+            else if (_state == GameState.battle)
+            {
+                _spriteBatch.Draw(_player, new Vector2(100, 200), _playerSrc[0], Color.White);
+                _spriteBatch.Draw(_icons, new Vector2(100, 350), Color.White);
+            }
             _spriteBatch.End();
             base.Draw(gameTime);
         }
